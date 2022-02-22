91e08fe364c56d954a8d8e9934b30247047f8ce1
Victor Hugo <victorhsmsantos@gmail.com>
2022-02-22 18:46:55 -0300

    Requisito 4 feito!

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const title = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+
+    expect(title).toBeDefined();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+
+    const image = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(image.src).toBe(URL);
+  });
+});
